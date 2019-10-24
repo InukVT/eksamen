e7d82befe6b59a53709002c89c1e07cf17dd876b
Bastian Inuk Christensen <inuk@ruc.dk>
2019-10-24 10:59:27 +0200

    Made it so we can append items to players inventory, with constraint.
    Signed-off-by: Bastian Inuk Christensen <bastian@inuk.blog>

--- src/blog/inuk/Item.java
+++ src/blog/inuk/Item.java
@@ -3,4 +3,5 @@ package blog.inuk;
 public class Item {
     /** Items name */
     public String name;
+    public  int weight;
 }
--- src/blog/inuk/Player.java
+++ src/blog/inuk/Player.java
@@ -1,4 +1,32 @@
 package blog.inuk;
 
+import java.util.ArrayList;
+
 public class Player {
+    public static ArrayList<Item> inventory;
+
+    /** How much the player can carry */
+    private static int carryWeight = 64;
+
+    /** Adds an item to the players inventory, if the player can handle the weight. Returns true if successfull, otherwise false. */
+    public static boolean appendInventory(Item item){
+        if(canCarry(item)){
+            inventory.add(item);
+            return true;
+        }
+        return false;
+    }
+
+    /** Calculates weather the player can carry the passed in item */
+    private static boolean canCarry(Item itemToAdd){
+        int isCarrying = 0;
+        for (Item item : inventory){
+            isCarrying += item.weight;
+
+        }
+        if(isCarrying + itemToAdd.weight <= carryWeight){
+            return true;
+        }
+        return false;
+    }
 }
