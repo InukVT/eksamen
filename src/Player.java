e2404209b1590a6331ebdd081f4d67606c33862a
Bastian Inuk Christensen <bastian@inuk.blog>
2019-10-25 14:37:25 +0200

    Made it so that HP is private
    Signed-off-by: Bastian Inuk Christensen <bastian@inuk.blog>

--- src/Player.java
+++ src/Player.java
@@ -1,14 +1,15 @@
-
 import java.util.ArrayList;
 
 /** The player class
  * @author Bastian Inuk Christensen: inuk@ruc.dk
  * */
 public class Player {
+
     /** What the player carries */
-    public ArrayList<Item> inventory;
+    private ArrayList<Item> inventory;
     /** Players health */
-    public int hp;
+    private int hp;
+
     /** Name og Player */
     public String name;
 
@@ -20,19 +21,30 @@ public class Player {
     public Player(String name){
         this.carryWeight = 64;
         this.name = name;
+        this.hp = 20;
     }
 
     /** Player represented as a String
      *  <p>
      *  TODO: Find a better representation of player, like health
      *
-     *  @return Players name, as that represent the player the best
+     *  @return {@link Player}'s {@link #name}, as that represent the player the best
      *
      *  */
     @Override
-
     public String toString() {
         return name;
+    }
+
+    /** @return {@link #hp}*/
+    public int getHp() {
+        return hp;
+    }
+
+    /** Damage player
+     *  @param amount the amount of health point to remove from the player*/
+    public void damage(int amount){
+        this.hp -= amount;
     }
 
     /** Adds an item to the players inventory, if the player can handle the weight. Returns true if successfull, otherwise false.
@@ -41,9 +53,9 @@ public class Player {
      *
      * @return <code>true</code> if the item added <code>false</code> if the item is too heavy for the user, and thus can't be added.
      * */
-    public static boolean appendInventory(Item item){
+    public boolean appendInventory(Item item){
         if(canCarry(item)){
-            inventory.add(item);
+            this.inventory.add(item);
             return true;
         }
         return false;
@@ -56,7 +68,14 @@ public class Player {
        inventory.remove(item);
     }
 
-    /** Calculates weather the player can carry the passed in item */
+    /** @return a list of {@link Item} the user is carrying. */
+    public ArrayList<Item> getInventory() {
+        return inventory;
+    }
+
+    /** Calculates weather the player can carry the passed in item
+     * @param itemToAdd the item to calculate if is light enough to be added to the inventory
+     * @return true if the item can be added, false if too heavy*/
     private static boolean canCarry(Item itemToAdd){
         int isCarrying = 0;
         for (Item item : inventory){
