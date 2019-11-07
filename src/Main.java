32d97cd8a6f355fb5ec840d03038fb03a7f6fe5d
Bastian Inuk Christensen <bastian@inuk.blog>
2019-11-07 11:01:07 +0100

    Initialise the player

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
+        // TODO: Ask user for name, handle exceptions
+        String name = "John";
+        Player player = new Player(name);
 
+        playerInfoString(player.getHp(), player.name);
+    }
+    private static String playerInfoString(int hp, String name) {
+        return String.format("Hello %s, you currently have %i HP", name, hp);
     }
 }
