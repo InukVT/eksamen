33acdb6a78a0684da1efa16571229ca172e2789b
awork <awork16@gmail.com>
2019-11-15 11:49:04 +0100

    Added toString (west, east, north & south)
    Signed-off-by: awork <awork16@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -51,11 +51,11 @@ public class Main {
         String name = "John";
         Player player = new Player(name);
 
-        playerInfoString(player.getHp(), player.name);
+      //  playerInfoString(player.getHp(), player.name);
     }
-    private static String playerInfoString(int hp, String name) {
-        return String.format("Hello %s, you currently have %i HP", name, hp);
-    }
+   // private static String playerInfoString(int hp, String name) {
+      //  return String.format("Hello %s, you currently have %i HP", name, hp);
+//    }
 
     /** Convenience function over std out */
     public static void println(String toPrint){
