e99470f35f67be675d8e727f73c75cb72ca4f189
Bastian Inuk Christensen <bastian@inuk.blog>
2019-11-15 10:02:00 +0100

    Init combat loop

--- src/Combat.java
+++ src/Combat.java
@@ -19,4 +19,12 @@ public class Combat {
         receiver.damage(damage);
         return receiver;
     }
+
+    /** The combat loop */
+    public void combatLoop(Creature player, Creature enemy){
+        Main.println(String.format("You've encountered %s, they have %i health, and you have %i. What are you going to do?", enemy.getName(), enemy.getHp(), player.getHp()));
+        while (player.getHp() > 0 && enemy.getHp() > 0){
+
+        }
+    }
 }
