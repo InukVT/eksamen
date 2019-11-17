aee8828a105f047452c01e01495f0ce5c8f8a65c
awork <awork16@gmail.com>
2019-11-17 15:06:34 +0100

    Remove comment

--- src/Item.java
+++ src/Item.java
@@ -4,7 +4,7 @@ public class Item {
     /**
      * Items name
      */
-    
+
     public ArrayList<Item> weapons = new ArrayList<Item>();
     public ArrayList<Item> potions = new ArrayList<Item>();
     public ArrayList<Item> keys = new ArrayList<Item>();
