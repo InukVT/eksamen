32fc7295c7e26a36ea3283dcac677f359a82d837
psychosaiyan <kaiyin62@live.dk>
2019-11-17 12:29:51 +0100

    Squashed commit of the following:
    commit 1eb4688191d78410b67c9ca691d7f428e920ecb1
    Author: Bastian Inuk Christensen <bastian@inuk.blog>
    Date:   Fri Nov 15 14:49:07 2019 +0100
    
        Init bibliography
    
    commit 8ae6c8712e415ded9a6e47a648472ae5092ad2f6
    Author: awork <awork16@gmail.com>
    Date:   Fri Nov 15 13:47:48 2019 +0100
    
        changed engine to report
    
    commit 49eda49809c09c2afbccc59a1882b3f724211f49
    Author: Bastian Inuk Christensen <bastian@inuk.blog>
    Date:   Fri Nov 15 13:34:07 2019 +0100
    
        commit issue

--- src/Combat.java
+++ src/Combat.java
@@ -9,11 +9,13 @@ public class Combat {
         return hitRate;
     }
 
-    /** Returns the amount of damage, attacker is giving
+    /** Returns creature
+     * TODO: Make it return damage instead, and when called, call it from {@link Creature}
+     * @param receiver the {@link Creature} getting attacked
      * @param sender the {@link Creature} attacking
-     * @return the amount of damage dealt by sender
+     * @return
      */
-    public int hit(Creature sender) {
+    public int hit(Creature receiver, Creature sender) {
        int hitRate = hitRate();
        int defRate = hitRate();
        if (hitRate > defRate) {
@@ -29,17 +31,14 @@ public class Combat {
            System.out.println("You missed");
            return 0;
        }
-        return 0;
+       return 0;
     }
 
     /** The combat loop */
     public void combatLoop(Creature player, Creature enemy){
         Main.println(String.format("You've encountered %s, they have %i health, and you have %i. What are you going to do?", enemy.getName(), enemy.getHp(), player.getHp()));
         while (player.getHp() > 0 && enemy.getHp() > 0){
-            Main.println("Choose your attack!");
-            // TODO: Add choice of action
-            player.damage(hit(enemy));
-            enemy.damage(hit(player));
+
         }
     }
 }
