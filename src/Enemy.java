b5c719727740fa873c4f52e91c9bf01cd081f4d8
psychosaiyan <kaiyin62@live.dk>
2019-11-18 14:25:36 +0100

    rebalanced nurse back 50hp

--- src/Enemy.java
+++ src/Enemy.java
@@ -7,7 +7,7 @@ public class Enemy extends Creature {
 
     /** special enemies */
     static Enemy AngryMan = new Enemy ("Angry Man", 60, Item.generateAllItems().get(5)); // Mini Boss
-    static Enemy Nurse = new Enemy ("Nurse", 20, Item.generateAllItems().get(5)); // Boss for potions
+    static Enemy Nurse = new Enemy ("Nurse", 50, Item.generateAllItems().get(5)); // Boss for potions
     static Enemy VeryAngryMan = new Enemy("Very Angry Man", 70, Item.generateAllItems().get(5)); //Boss
 }
 
