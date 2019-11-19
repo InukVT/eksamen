698b2ab96c134d89c1fbdb578bbf7e4d343e472d
psychosaiyan <kaiyin62@live.dk>
2019-11-19 20:35:12 +0100

    just let me have it >:(

--- src/Enemy.java
+++ src/Enemy.java
@@ -5,6 +5,7 @@ public class Enemy extends Creature {
     }
 
     // Sepcial enemies
+    // Each enemy is an instance.
     static Enemy AngryMan = new Enemy ("Angry Man", 60, Item.generateAllItems().get(7)); // Mini Boss
     static Enemy Nurse = new Enemy ("Nurse", 50, Item.generateAllItems().get(9)); // Boss for potions
     static Enemy VeryAngryMan = new Enemy("Very Angry Man", 150, Item.generateAllItems().get(8)); //Boss
